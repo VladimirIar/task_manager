0792543a7237d6f2bdb7ecf1f1b654fa45c55298
VladimirIar <yarvoldem@yandex.ru>
2026-09-03 20:38:15 +0300

    feature: time at notification

--- app/task_notifications.py
+++ app/task_notifications.py
@@ -1,6 +1,9 @@
 import time
+from datetime import datetime
 
 def send_notification(task_id: int, task_title: str):
   time.sleep(2)
+
+  now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
   with open("notifications.log", "a", encoding="utf-8") as f:
-    f.write(f'Уведомление: Создана задача #{task_id} - "{task_title}" \n')
+    f.write(f'[{now}] Уведомление: Создана задача #{task_id} - "{task_title}" \n')
